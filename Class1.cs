604f162c8ba4cb3285988a70f6d871c75711ff5e
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-18 14:32:07 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Persona
+    {
+        private string nome;
+        private string cognome;
+        private int eta;
+
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+        public string GetNome()
+        { return nome; }
+
+        public string GetCognome()
+        { return cognome; }
+
+        public int Geteta()
+        { return eta; }
+
+        public string GetDettagli()
+        { return "nome:" + GetNome() + " cognome: " + GetCognome() + " età: " + Geteta(); }
+
+        
+        
+        
+        
+        
+        
+      
+
+       
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("mario", "Rossi", 25);
+            Console.WriteLine(persona.GetDettagli());
+
+            Persona persona2 = new Persona("Giovanni", "Muchaccha", 25);
+            Console.WriteLine(persona2.GetDettagli());
+
+
+
+
+
+        }
+    }
+}
